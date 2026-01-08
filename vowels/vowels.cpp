b8cb0c0369592ba83f7b050d49aa22ffe47c9a06
krish shinde <krishshinde@krishs-MacBook-Air.local>
2026-01-08 10:59:11 +0530

    nested files

---
+++ vowels/vowels.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    cout << "Enter a character: ";
+    cin >> ch;
+
+    switch(ch) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+            cout << ch << " is a vowel." << endl;
+            break;
+        default:
+            cout << ch << " is not a vowel." << endl;
+    }
+    return 0;   
+}
